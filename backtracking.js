2bc8b16dbab1b01cee28bf7b9425cef07e320fdc
woodylucas <wlucas0311@gmail.com>
2021-04-20 19:44:20 -0400

    solved nqueens

--- backtracking.js
+++ backtracking.js
@@ -69,7 +69,109 @@ const maze = [
   [0, 1, 1, 1],
 ];
 
-console.log(solveMaze(maze));
+// console.log(solveMaze(maze));
+
+// N-Queens 1
+/* 
+Choices: to place a queen we can either go row by row column by column we will be going by column
+Constraints: A queen can not be place horizontally, diagonally, and vertcally to another queen
+Recursion: Choice is safe to be made place in row into column, if choices do not work out, discard the current move
+Goal: Place the Queen in the right place, until we reach the last column
+
+*/
+
+function nonAttackingQueen(n) {
+  const result = [];
+  const colPlacements = new Array(n).fill(0);
+  getNonAttackingQueens(0, colPlacements, n, result);
+  return result;
+}
+
+function getNonAttackingQueens(row, colPlacements, boardSize, result) {
+  // Goal
+  if (row === boardSize) {
+    const board = generateBoard(boardSize, colPlacements);
+    result.push(board);
+    return;
+  }
+
+  for (let col = 0; col < boardSize; col++) {
+    if (isNonAttackPlacements(row, col, colPlacements)) {
+      colPlacements[row] = col;
+      getNonAttackingQueens(row + 1, colPlacements, boardSize, result);
+    }
+  }
+}
+
+function isNonAttackPlacements(row, col, colPlacements) {
+  for (let previousRow = 0; previousRow < row; previousRow++) {
+    const columnCheck = colPlacements[previousRow];
+    const sameColumn = columnCheck === col;
+    const onDiagonal = Math.abs(columnCheck - col) === row - previousRow;
+    if (onDiagonal || sameColumn) return false;
+  }
+  return true;
+}
+
+function generateBoard(boardSize, colPlacements) {
+  const board = [];
+  for (let row = 0; row < colPlacements.length; row++) {
+    const boardRow = [];
+    for (let col = 0; col < boardSize; col++) {
+      if (col === colPlacements[row]) {
+        boardRow.push("Q");
+      } else {
+        boardRow.push(".");
+      }
+    }
+    board.push(boardRow.join(""));
+  }
+  return board;
+}
+
+// console.log(nonAttackingQueen(4));
+
+// Choices: Placing n queens either row by row or columnn by column the length of the board
+// Constraints: A isNonAttackingQueen can not be placed either horizonatally, vertically, or diagonally
+// Decisions/ Recursion: If position is safe we will place the position in col else we undo current decision
+// Goal: Once we have placed all queens in n available position we will increment our counter when we reach the lets column on the board
+function totalNQueens(n) {
+  const colPlacements = new Array(n).fill(0);
+  return getTotalOfNonAttackingPlacments(0, n, colPlacements);
+}
+
+function getTotalOfNonAttackingPlacments(row, boardSize, colPlacements) {
+  // Goal
+  if (row === boardSize) {
+    return 1;
+  }
+
+  let validPlacements = 0;
+  // Constraints and Choices
+  for (let col = 0; col < boardSize; col++) {
+    if (isNonAttackPlacementsClone(row, col, colPlacements)) {
+      colPlacements[row] = col; // Our choice
+      validPlacements += getTotalOfNonAttackingPlacments(
+        row + 1,
+        boardSize,
+        colPlacements
+      );
+    }
+  }
+  return validPlacements;
+}
+
+function isNonAttackPlacementsClone(row, col, colPlacements) {
+  for (let previousRow = 0; previousRow < row; previousRow++) {
+    const columnToCheck = colPlacements[previousRow];
+    const sameColumn = col === columnToCheck;
+    const onDiagonal = Math.abs(columnToCheck - col) === row - previousRow;
+    if (onDiagonal || sameColumn) return false;
+  }
+  return true;
+}
+
+console.log(totalNQueens(4));
 
 // Choices is to include or exclude
 // constraints must not contain duplicates
